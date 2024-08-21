0db87da8cc3c73745f5cb213509675c4138ac43d
zotov.andrey.2008@gmail.com <zotov.andrey.2008@gmail.com>
2024-08-21 12:24:16 +0300

    Add project files.

---
+++ GLibTest_CSDZ7/Program.cs
@@ -0,0 +1,17 @@
+﻿using GeometryLib;
+
+var circle = new Circle(5);
+var rectangle = new Rectangle(4, 6);
+var triangle = new Triangle(3, 4, 5);
+
+Console.WriteLine(circle);
+Console.WriteLine($"Area: {circle.Area()}");
+Console.WriteLine($"Perimeter: {circle.Perimeter()}");
+
+Console.WriteLine(rectangle);
+Console.WriteLine($"Area: {rectangle.Area()}");
+Console.WriteLine($"Perimeter: {rectangle.Perimeter()}");
+
+Console.WriteLine(triangle);
+Console.WriteLine($"Area: {triangle.Area()}");
+Console.WriteLine($"Perimeter: {triangle.Perimeter()}");
